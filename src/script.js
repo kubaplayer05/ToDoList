6df626671328e9db541f60b799c7c2fb200e53bf
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2022-10-10 18:04:45 +0200

    remove categories, add delete btn, remove edit btn

--- src/script.js
+++ src/script.js
@@ -5,11 +5,12 @@ const taskModal = document.querySelector('.task-modal')
 // get all buttons from menu
 const createBtn = document.querySelector('.create-btn')
 const editBtn = document.querySelector('.edit-btn')
-const deleteBtn = document.querySelector('.delete-btn')
+const deleteBtn = document.querySelector('.restart-btn')
 const settingsBtn = document.querySelector('.settings-btn')
 // main modal
 const inputTitle = document.querySelector('#input-title')
 const inputDesc = document.querySelector('#input-desc')
+const errorMsg = document.querySelector('.form-error')
 const submit = document.querySelector('.form-submit')
 // icon
 const closeIcon = document.querySelectorAll('.fa-x')
@@ -38,9 +39,11 @@ closeIcon.forEach(icon => {
 })
 
 submit.addEventListener('click', () => {
-   createTask(getDataFromInputs().title,getDataFromInputs().desc)
-   clearTaskModal()
-   closeTaskModal()
+    if(taskValidation()) {
+        createTask(getDataFromInputs().title,getDataFromInputs().desc)
+        clearTaskModal()
+        closeTaskModal()
+    }
 })
 
 thmeCards.forEach(card => {
@@ -51,6 +54,10 @@ thmeCards.forEach(card => {
     })
 })
 
+deleteBtn.addEventListener('click',() => {
+    deleteAllTasks()
+})
+
 // functions
 
 const createTask = (titleValue,descValue) => {
@@ -58,24 +65,18 @@ const createTask = (titleValue,descValue) => {
     const task = document.createElement('div')
     const title = document.createElement('h2')
     const desc = document.createElement('p')
-    const categoryDiv = document.createElement('div')
-    const categoryTitle = document.createElement('span')
     const checkbox = document.createElement('input')
     // assign classes and attributes
     task.classList.add('task')
     title.classList.add('task-title')
     desc.classList.add('task-desc')
-    categoryDiv.classList.add('task-category')
-    categoryTitle.classList.add('category-title')
     checkbox.classList.add('task-checkbox')
     checkbox.setAttribute('type','checkbox')
     // set text-content
     title.textContent = titleValue
     desc.textContent = descValue
-    categoryTitle.textContent = 'Category'
     // appends
-    categoryDiv.append(categoryTitle)
-    task.append(title,desc,categoryDiv,checkbox)
+    task.append(title,desc,checkbox)
     container.append(task)
 }
 
@@ -102,6 +103,7 @@ const closeTaskModal = () => {
 const clearTaskModal = () => {
     inputTitle.value = ''
     inputDesc.value = ''
+    errorMsg.style.opacity = '0'
 }
 
 const showUpTaskModal = () => {
@@ -112,6 +114,19 @@ const showUpTaskModal = () => {
 const showUpSettingsModal = () => {
     settingsModal.style.display = 'flex'
     shadow.style.display = 'block'
+}
+
+const taskValidation = () => {
+    if(inputTitle.value ==='') {
+        errorMsg.style.opacity = '1'
+        return false
+    } else return true
+}
+
+const deleteAllTasks = () => {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+      }
 }
 
 const loadThemeColor = () => {
